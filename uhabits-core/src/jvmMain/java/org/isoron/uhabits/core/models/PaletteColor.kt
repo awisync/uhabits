ac688622b724793f26fd7aedaa95f906374c38ba
awisync <awishelf@gmail.com>
2026-03-16 11:32:02 +0530

    Update PaletteColor.kt

--- uhabits-core/src/jvmMain/java/org/isoron/uhabits/core/models/PaletteColor.kt
+++ uhabits-core/src/jvmMain/java/org/isoron/uhabits/core/models/PaletteColor.kt
@@ -16,32 +16,60 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.isoron.uhabits.core.models
 
 data class PaletteColor(val paletteIndex: Int) {
     fun toCsvColor(): String {
         return arrayOf(
-            "#D32F2F", //  0 red
-            "#E64A19", //  1 deep orange
-            "#F57C00", //  2 orange
-            "#FF8F00", //  3 amber
-            "#F9A825", //  4 yellow
-            "#AFB42B", //  5 lime
-            "#7CB342", //  6 light green
-            "#388E3C", //  7 green
-            "#00897B", //  8 teal
-            "#00ACC1", //  9 cyan
-            "#039BE5", // 10 light blue
-            "#1976D2", // 11 blue
-            "#303F9F", // 12 indigo
-            "#5E35B1", // 13 deep purple
-            "#8E24AA", // 14 purple
-            "#D81B60", // 15 pink
-            "#5D4037", // 16 brown
-            "#303030", // 17 dark grey
-            "#757575", // 18 grey
-            "#aaaaaa" // 19 light grey
+            "#D32F2F", //  0 red_700
+            "#B71C1C", //  1 red_900
+            "#D81B60", //  2 pink_600
+            "#880E4F", //  3 pink_800
+            "#E64A19", //  4 deep_orange_700
+            "#BF360C", //  5 deep_orange_900
+            "#F57C00", //  6 orange_700
+            "#E65100", //  7 orange_900
+            "#FF8F00", //  8 amber_800
+            "#F9A825", //  9 yellow_800
+            "#F57F17", // 10 yellow_900
+            "#AFB42B", // 11 lime_700
+            "#827717", // 12 lime_900
+            "#7CB342", // 13 light_green_600
+            "#558B2F", // 14 light_green_800
+            "#388E3C", // 15 green_700
+            "#1B5E20", // 16 green_900
+            "#009688", // 17 teal_500
+            "#00897B", // 18 teal_600
+            "#00796B", // 19 teal_700
+            "#004D40", // 20 teal_900
+            "#00ACC1", // 21 cyan_600
+            "#0097A7", // 22 cyan_700
+            "#006064", // 23 cyan_900
+            "#039BE5", // 24 light_blue_600
+            "#0277BD", // 25 light_blue_800
+            "#1976D2", // 26 blue_700
+            "#0D47A1", // 27 blue_900
+            "#3949AB", // 28 indigo_600
+            "#303F9F", // 29 indigo_700
+            "#1A237E", // 30 indigo_900
+            "#5E35B1", // 31 deep_purple_600
+            "#4527A0", // 32 deep_purple_800
+            "#8E24AA", // 33 purple_600
+            "#6A1B9A", // 34 purple_800
+            "#4A148C", // 35 purple_900
+            "#6D4C41", // 36 brown_500
+            "#5D4037", // 37 brown_700
+            "#3E2723", // 38 brown_900
+            "#546E7A", // 39 blue_grey_500
+            "#455A64", // 40 blue_grey_600
+            "#37474F", // 41 blue_grey_700
+            "#263238", // 42 blue_grey_800
+            "#9E9E9E", // 43 grey_500
+            "#757575", // 44 grey_600
+            "#616161", // 45 grey_700
+            "#424242", // 46 grey_800
+            "#212121", // 47 grey_900
+            "#101010"  // 48 grey_950
         )[paletteIndex]
     }
 
