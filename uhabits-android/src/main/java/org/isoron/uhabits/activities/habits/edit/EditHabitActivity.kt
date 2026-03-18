8af2b42f84a987fb86fe93badcfc05e254f4fb1f
awisync <awishelf@gmail.com>
2026-03-18 09:47:14 +0530

    Update EditHabitActivity.kt

--- uhabits-android/src/main/java/org/isoron/uhabits/activities/habits/edit/EditHabitActivity.kt
+++ uhabits-android/src/main/java/org/isoron/uhabits/activities/habits/edit/EditHabitActivity.kt
@@ -58,6 +58,7 @@ import org.isoron.uhabits.databinding.ActivityEditHabitBinding
 import org.isoron.uhabits.utils.applyRootViewInsets
 import org.isoron.uhabits.utils.applyToolbarInsets
 import org.isoron.uhabits.utils.formatTime
+import org.isoron.uhabits.utils.dismissCurrentAndShow
 
 fun formatFrequency(freqNum: Int, freqDen: Int, resources: Resources) = when {
     freqNum == 1 && (freqDen == 30 || freqDen == 31) -> resources.getString(R.string.every_month)
